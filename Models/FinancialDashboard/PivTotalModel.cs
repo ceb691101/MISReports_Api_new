1a7d95484c0833b2d503d6f7a388fef1744e3af0
Chanuka Wimalasiri <chanuka636@gmail.com>
2026-04-23 12:17:58 +0530

    feat: implement financial dashboard data access objects and models

---
+++ Models/FinancialDashboard/PivTotalModel.cs
@@ -0,0 +1,8 @@
+namespace MISReports_Api.Models.FinancialDashboard
+{
+    public class PivTotalModel
+    {
+        public string date { get; set; }
+        public double amount { get; set; }
+    }
+}
