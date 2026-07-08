5936aeae7473c47cc55451050442f2783232ebf2
nethma-dewmini <rathnayakarmnd.22@uom.lk>
2026-07-08 10:09:36 +0530

    Centralize ConvertMonthNameToNumber utility inside BillCycleHelper to reduce code duplication in the DAOs

--- DAL/PUCSLReports/PUCSLSolarConnection/SolarDataUNTCalculationDao.cs
+++ DAL/PUCSLReports/PUCSLSolarConnection/SolarDataUNTCalculationDao.cs
@@ -411,7 +411,7 @@ namespace MISReports_Api.DAL.PUCSLReports.PUCSLSolarConnection
                 {
                     string monthName = parts[0];
                     string year = parts[1];
-                    string monthNumber = ConvertMonthNameToNumber(monthName);
+                    string monthNumber = BillCycleHelper.ConvertMonthNameToNumber(monthName);
                     return (year, monthNumber);
                 }
 
@@ -421,26 +421,6 @@ namespace MISReports_Api.DAL.PUCSLReports.PUCSLSolarConnection
             {
                 logger.Error(ex, $"Error parsing bill cycle {billCycle}");
                 return ("", "");
-            }
-        }
-
-        private string ConvertMonthNameToNumber(string monthName)
-        {
-            switch (monthName)
-            {
-                case "Jan": return "1";
-                case "Feb": return "2";
-                case "Mar": return "3";
-                case "Apr": return "4";
-                case "May": return "5";
-                case "Jun": return "6";
-                case "Jul": return "7";
-                case "Aug": return "8";
-                case "Sep": return "9";
-                case "Oct": return "10";
-                case "Nov": return "11";
-                case "Dec": return "12";
-                default: return monthName;
             }
         }
     }
