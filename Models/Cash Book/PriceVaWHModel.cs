f0970f20b4b79633fc37a525aec2cf298b31a4b9
bhagyanawarathna <bhagya0913@gmail.com>
2026-07-15 14:41:05 +0530

    Price Variance WH wise Report

---
+++ Models/Cash Book/PriceVaWHModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace MISReports_Api.Models.Accounts
+{
+    public class PriceVaWHModel
+    {
+        public string WrhCd { get; set; }
+        public string MatCd { get; set; }
+        public string GradeCd { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? NewPrice { get; set; }
+        public decimal? NetChange { get; set; }
+        public decimal? QtyOnHand { get; set; }
+        public decimal? Var { get; set; }
+        public string CctName { get; set; }
+    }
+}
