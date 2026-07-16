280df001ba259611fd5174a649e3f1b05a3bd8eb
bhagyanawarathna <bhagya0913@gmail.com>
2026-07-16 10:43:50 +0530

    Add Cheque Summary Report

---
+++ Models/Cash Book/ChequeSummaryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace MISReports_Api.Models.Accounts
+{
+    public class ChequeSummaryModel
+    {
+        public DateTime? ChqDt { get; set; }
+        public string ChqNo { get; set; }
+        public decimal? ChqAmt { get; set; }
+        public string CctName { get; set; }
+    }
+}
