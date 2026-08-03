10aad79ca5d33a174f64b9f665994c079339ee60
bhagyanawarathna <bhagya0913@gmail.com>
2026-08-03 13:54:09 +0530

    Add CCT1T2T3 Report

---
+++ Models/SMC- Management Information/CCT1T2T3Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MISReports_Api.Models.Accounts
+{
+    public class CCT1T2T3Model
+    {
+        public string ApplicationNo { get; set; }
+        public string ApplicationId { get; set; }
+        public string ProjectNo { get; set; }
+        public DateTime? AccCreatedDate { get; set; }
+        public DateTime? Piv1Date { get; set; }
+        public DateTime? ApprovalDate { get; set; }
+        public decimal? EstimateCost { get; set; }
+        public DateTime? Piv2Date { get; set; }
+        public DateTime? EnergizedDate { get; set; }
+        public decimal? T1 { get; set; }
+        public decimal? T2Ln { get; set; }
+        public decimal? T2Smc { get; set; }
+        public decimal? T3 { get; set; }
+        public string Loan { get; set; }
+        public string CctName { get; set; }
+    }
+}
