98af6d129733f86c81bdbdf0309fd2e8bfac1939
bhagyanawarathna <bhagya0913@gmail.com>
2026-09-01 10:25:50 +0530

    Update Code

--- Controllers/SMC/EnergizeAgeAnalysisController.cs
+++ Controllers/SMC/EnergizeAgeAnalysisController.cs
@@ -17,7 +17,7 @@ namespace MISReports_Api.Controllers
 
         private static readonly string[] DateFormats = { "yyyy/MM/dd", "yyyy-MM-dd" };
 
-        // QUERY: /api/energizeageanalysis/report?fromDate=2026/01/01&toDate=2026/01/31&costCtr=511.20
+        // QUERY: /api/energizeageanalysis/report?fromDate=2026/01/01&toDate=2026/03/31&costCtr=511.20
         [HttpGet]
         [Route("report")]
         public IHttpActionResult GetQuery([FromUri] string fromDate, [FromUri] string toDate, [FromUri] string costCtr)
