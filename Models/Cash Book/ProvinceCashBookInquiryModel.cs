44a1ce380cbdfd638db28175a94fdfe20327df21
Dinuli-Disara <disara184@gmail.com>
2026-09-01 10:37:52 +0530

    Add new APIs for Province Cash Book Inquiry
    Co-authored-by: BhagyaNawarathna <bhagya0913@gmail.com>

---
+++ Models/Cash Book/ProvinceCashBookInquiryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MISReports_Api.Models.Accounts
+{
+    public class ProvinceCashBookInquiryModel
+    {
+        public string Category { get; set; }
+        public string DeptId { get; set; }
+        public DateTime? DocDt { get; set; }
+        public decimal? NonTaxabl { get; set; }
+        public string DocNo { get; set; }
+        public string ApprvUid1 { get; set; }
+        public string ApprDt1 { get; set; }
+        public string TranStatus { get; set; }
+        public string Payee { get; set; }
+        public string ChqDt { get; set; }
+        public string ChqNo { get; set; }
+        public string PymtDocNo { get; set; }
+        public string PpStatus { get; set; }
+        public string BranchName { get; set; }
+    }
+}
