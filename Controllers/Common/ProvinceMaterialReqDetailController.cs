628ccf168567414d129eb954b40084ad78204fb9
Dinuli-Disara <disara184@gmail.com>
2026-09-02 12:16:20 +0530

    Add new API for Province wise Material Requisition Details report

---
+++ Controllers/Common/ProvinceMaterialReqDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MISReports_Api.Controllers.Common
+{
+    public class ProvinceMaterialReqDetailController : Controller
+    {
+        // GET: ProvinceMaterialReqDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
