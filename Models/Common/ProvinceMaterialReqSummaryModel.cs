0a0e4dbff63b392e399a27d086af5f8e66b09878
Dinuli-Disara <disara184@gmail.com>
2026-09-02 12:17:31 +0530

    Add new API for Province wise Material Requisition Summary report

---
+++ Models/Common/ProvinceMaterialReqSummaryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MISReports_Api.Models.Accounts
+{
+    public class ProvinceMaterialReqSummaryModel
+    {
+        public string TranStatus { get; set; }
+        public string DeptId { get; set; }
+        public int NoOfDocuments { get; set; }
+        public string BranchName { get; set; }
+    }
+}
