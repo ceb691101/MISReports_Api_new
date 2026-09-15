f257ca3c057fd09a6880c3f5467dfea17c12f8f7
Samadhi Jagathsiri <anusarasamm@gmail.com>
2026-09-15 11:11:17 +0530

    Add new API for  Material Price for given Year

---
+++ Controllers/Inventory/MaterialPriceByYearController.cs
@@ -0,0 +1,75 @@
+﻿using MISReports_Api.DAL.Inventory;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Web.Http;
+
+namespace MISReports_Api.Controllers
+{
+    [RoutePrefix("misapi/api/material-price")]
+    public class MaterialPriceByYearController : ApiController
+    {
+        private readonly MaterialPriceByYearDAL _materialPriceByYearDAL =
+            new MaterialPriceByYearDAL();
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetMaterialPriceByYear(
+            [FromUri] string costCtr,
+            [FromUri] string repYear)
+        {
+            if (string.IsNullOrWhiteSpace(costCtr))
+            {
+                var errorResponse = new
+                {
+                    data = (object)null,
+                    errorMessage = "Cost center is required."
+                };
+
+                return Ok(JObject.Parse(
+                    JsonConvert.SerializeObject(errorResponse)));
+            }
+
+            if (string.IsNullOrWhiteSpace(repYear))
+            {
+                var errorResponse = new
+                {
+                    data = (object)null,
+                    errorMessage = "Report year is required."
+                };
+
+                return Ok(JObject.Parse(
+                    JsonConvert.SerializeObject(errorResponse)));
+            }
+
+            try
+            {
+                var materialPrices =
+                    _materialPriceByYearDAL.GetMaterialPriceByYear(
+                        costCtr,
+                        repYear);
+
+                var response = new
+                {
+                    data = materialPrices,
+                    errorMessage = (string)null
+                };
+
+                return Ok(JObject.Parse(
+                    JsonConvert.SerializeObject(response)));
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new
+                {
+                    data = (object)null,
+                    errorMessage = "Cannot get material price details.",
+                    errorDetails = ex.Message
+                };
+
+                return Ok(JObject.Parse(
+                    JsonConvert.SerializeObject(errorResponse)));
+            }
+        }
+    }
+}
